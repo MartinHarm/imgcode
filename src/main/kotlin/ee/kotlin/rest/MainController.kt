539c90cd0ccbf3155e4ee169d3057f4a23ebf632
Martin Härm <martin.harm@proekspert.ee>
2016-11-04 14:58:48 +0200

    Built logic for getting each pixel color. Fixed testing client.

--- src/main/kotlin/ee/kotlin/rest/MainController.kt
+++ src/main/kotlin/ee/kotlin/rest/MainController.kt
@@ -2,6 +2,8 @@ package ee.kotlin.rest
 
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.awt.Color
+import javax.imageio.ImageIO
 
 /**
  * Created by Martin Härm on 03.11.2016.
@@ -11,14 +13,29 @@ class MainController {
 
     @CrossOrigin
     @RequestMapping(value = "/", method = arrayOf(RequestMethod.POST))
-    fun fileSize(@RequestBody multipartFile: MultipartFile): String {
-        return multipartFile.contentType+multipartFile.size
-    }
+    fun fileSize(@RequestParam("file") multipartFile: MultipartFile): String {
+        val image = ImageIO.read(multipartFile.inputStream)
 
-    @CrossOrigin
-    @RequestMapping(value = "/", method = arrayOf(RequestMethod.GET))
-    fun greeting(): String {
-        return "TEST"
+        val xpixels=image.width-1
+
+        val ypixels=image.height-1
+
+        val sb: StringBuilder = StringBuilder()
+
+
+        for(yCord in 0..ypixels) {
+            for(xCord in 0..xpixels) {
+                //Since the image is grayscale black and white
+                //I can check how much red is in each pixel
+                sb.append(Color(image.getRGB(xCord, yCord)).red.toString())
+                sb.append("|")
+            }
+            sb.append("<br />")
+        }
+
+        sb.append("<br />")
+
+        return sb.toString()
     }
 
 }
